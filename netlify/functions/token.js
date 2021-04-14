4c54764bb12560faa241a3541d5297e73598b1e2
John Mitchell <johnthefawn91@gmail.com>
2021-04-13 20:08:01 -0400

    netlify token function

---
+++ netlify/functions/token.js
@@ -0,0 +1,38 @@
+var request = require('request');
+require('dotenv').config();
+
+function getToken(){
+  var client_id = process.env.SPOTIFY_CLIENT_ID;
+  var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization:
+        'Basic ' +
+        new Buffer(client_id + ':' + client_secret).toString('base64')
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(body.access_token);
+      }
+    });
+  });
+}
+
+exports.handler = async function(event, context) {
+  // your server-side functionality
+  var token = await getToken();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({token: token})
+  };
+}
